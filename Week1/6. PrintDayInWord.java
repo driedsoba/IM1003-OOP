e75665575fc41b76310e4db1dfebbfceebdcf80d
driedsoba <122796569+driedsoba@users.noreply.github.com>
2023-01-20 00:35:19 +0800

    Update 6. PrintDayInWord.java

--- Week1/6. PrintDayInWord.java
+++ Week1/6. PrintDayInWord.java
@@ -14,29 +14,22 @@ public class PrintDayInWord {
         //Using nested-if to handle the 8 cases (0 to 6 and invalid inputs)
         if (dayNumber == 0) {
             System.out.println("Sunday");
-            System.out.println("bye");
         } else if (dayNumber == 1) {
             System.out.println("Monday");
-            System.out.println("bye");
         } else if (dayNumber == 2) {
             System.out.println("Tuesday");
-            System.out.println("bye");
         } else if (dayNumber == 3) {
             System.out.println("Wednesday");
-            System.out.println("bye");
         } else if (dayNumber == 4) {
             System.out.println("Thursday");
-            System.out.println("bye");
         } else if (dayNumber == 5) {
             System.out.println("Friday");
-            System.out.println("bye");
         } else if (dayNumber == 6) {
             System.out.println("Saturday");
-            System.out.println("bye");
         } else {
             System.out.println("Not a valid day");
-            System.out.println("bye");
         }
+        System.out.println("bye");
         sc.close();
     }
 }

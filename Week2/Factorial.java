4fb4776b74337fde183642fe85abf2d065809fc6
driedsoba <122796569+driedsoba@users.noreply.github.com>
2023-01-28 01:37:35 +0800

    Add files via upload

---
+++ Week2/Factorial.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+
+        // Declaration of variables
+        int factorialInt, product = 1;
+        Scanner sc = new Scanner(System.in);
+
+        // Getting user input
+        System.out.println("Enter an integer: ");
+        factorialInt = sc.nextInt();
+
+        // Calculating factorial using for loop
+        for(int counter = 1; counter <= factorialInt; counter++) {
+            product *= counter;
+        }
+
+        // Printing output
+        System.out.println("The Factorial of " + factorialInt + " is: " + product);
+    }
+}

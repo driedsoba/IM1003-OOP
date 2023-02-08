452dc0a2e9f2a53d906c73b2f716a3f7812cb5d1
driedsoba <122796569+driedsoba@users.noreply.github.com>
2023-02-08 13:27:19 +0800

    Add files via upload

---
+++ Week 3/TriangularPatternC.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class TriangularPatternC {
+    public static void main(String[] args) {
+
+        // Declaration of variables
+        int size;
+        Scanner sc = new Scanner(System.in);
+
+        // User int input for size
+        System.out.println("Enter the size: ");
+        size = sc.nextInt();
+
+        // Printing number of stars based on user input
+        for (int row = 1; row <= size; row++) {
+
+            for (int col = 1; col <= size; col++) {
+                if (row <= col) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            // Print a newline after all the columns
+            System.out.println();
+        }
+    }
+}

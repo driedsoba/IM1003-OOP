7a30c5fb9a2d094aaf4d0c5584212b7f30f9bf3c
driedsoba <122796569+driedsoba@users.noreply.github.com>
2023-02-11 21:32:32 +0800

    Add files via upload

---
+++ Week3/ReverseString.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        // Variable Declaration
+        String inStr;   // input String
+        int inStrLen;   // length of the input String
+        Scanner in = new Scanner(System.in);
+
+        // User input for String
+        System.out.print("Enter a String: ");
+        inStr = in.next();   // use next() to read a String
+        inStrLen = inStr.length();
+
+        // Use inStr.charAt(index) in a loop to extract character at "index" from inStr.
+        // The String's index begins at 0 from the left.
+
+        for (int charIdx = inStrLen - 1; charIdx >= 0; charIdx--) {  // Process the String from the right
+            // Print the inStr.charAt(charIdx)
+            System.out.print(inStr.charAt(charIdx));
+        }
+    }
+}

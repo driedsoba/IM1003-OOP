c7e3064e1e6e1e658897ccf35369871d86e25fce
driedsoba <122796569+driedsoba@users.noreply.github.com>
2023-02-16 11:43:50 +0800

    Add files via upload

---
+++ Bin2Dec.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Bin2Dec {
+    public static void main(String[] args) {
+
+        // Declaration of variables
+        String binStr;
+        int binStrLen;
+        char binChar;
+        int dec = 0;
+        Scanner sc = new Scanner(System.in);
+
+        // Asking for input
+        System.out.println("Enter a binary string: ");
+        binStr = sc.next();
+        binStrLen = binStr.length();
+        sc.close();
+
+        // Processing String from left
+        for(int binStrIdx = 0; binStrIdx < binStrLen; binStrIdx++) {
+            // Extracting char
+            binChar = binStr.charAt(binStrIdx);
+
+            // Considering the 3 cases
+            if(binChar == '0') {
+                // Do nothing
+            } else if(binChar == '1') {
+                dec += (int)Math.pow(2, (binStrLen - 1 - binStrIdx));
+            } else {
+                System.err.println("Invalid Binary String");
+            }
+        }
+        System.out.println("The decimal equivalent is: " + dec);
+
+    }
+}

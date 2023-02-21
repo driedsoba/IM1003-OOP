18482abe633f9d4b8ada3f6d8b2fdbfb115821b8
driedsoba <122796569+driedsoba@users.noreply.github.com>
2023-02-22 00:40:42 +0800

    Update 9.1a PhoneKeyPad.java

--- Week3/9.1a PhoneKeyPad.java
+++ Week3/9.1a PhoneKeyPad.java
@@ -28,8 +28,8 @@ public class PhoneKeyPad {
                     // case 'a': case 'b': case 'c':
                     // same as: if (inChar == 'a' || inChar == 'b' || inChar == 'c')
                     case 'a': case 'b': case 'c': System.out.print(2); break;
-                    case 'd': case 'e': case 'f':System.out.print(3); break;
-                    case 'g': case 'h': case 'i':System.out.print(4); break;
+                    case 'd': case 'e': case 'f': System.out.print(3); break;
+                    case 'g': case 'h': case 'i': System.out.print(4); break;
                     case 'j': case 'k': case 'l': System.out.print(5); break;
                     case 'm': case 'n': case 'o': System.out.print(6); break;
                     case 'p': case 'q': case 'r': case 's':System.out.print(7); break;

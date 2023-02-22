a302afdb630e0a6aaa018dca99b8469628a708d8
driedsoba <122796569+driedsoba@users.noreply.github.com>
2023-02-22 15:44:59 +0800

    Update 9. GradesHistogramVertical.java

--- Week4/9. GradesHistogramVertical.java
+++ Week4/9. GradesHistogramVertical.java
@@ -2,70 +2,58 @@ import java.util.Scanner;
 
 public class GradesHistogramVertical {
     public static void main(String[] args) {
+        int numItems, lowerBin, upperBin, max = 0;
+        int[] items;
+        int[] numInBin;
 
-        // Declare variables
-        int numStudents, lowerBin, upperBin;
-        int[] grades;                                   // Declare array name, to be allocated after numStudents is known
-        int[] bins = new int[10];                       // int array of 10 histogram bins for 0-9, 10-19, ..., 90-100
-        Scanner sc = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number of students: ");
+        numItems = in.nextInt();
 
-        // Reading input for numStudents
-        System.out.println("Enter the number of students: ");
-        numStudents = sc.nextInt();
+        items = new int[numItems];
+        numInBin = new int[10];
 
-        // Allocate array
-        grades = new int[numStudents];
-
-        // Reading input for grades if numStudents > 0
-        if (numStudents > 0) {
-            for (int idx = 0; idx < numStudents; idx++) {
-                System.out.println("Enter the grade for student " + (idx + 1) + ": ");
-                grades[idx] = sc.nextInt();
+        if (numItems > 0) {
+            for (int index = 0; index < numItems; index++) {
+                System.out.print("Enter the grade for student " + (index + 1) + ": ");
+                items[index] = in.nextInt();
             }
         }
+        in.close();
 
         // Populate the bins
-        for (int i = 0; i < grades.length; ++i) {
-            if (grades[i] == 100) {                     /* Need to handle 90-100 separately as it has 11 items. So we consider 100
-                                                           as a case in itself */
-                ++bins[9];                              // eliminating this special case first by adding to bin outside of loop
-            } else {
-                ++bins[grades[i] / 10];                 // adding count to bins[0-9] with logic where 21/10 = 2 and 45/10 = 4
+        for (int index = 0; index < numItems; index++) {
+            if (items[index] == 100)
+                numInBin[9]++;
+            else {
+                numInBin[items[index] / 10]++;
             }
         }
-        // Print the vertical histograms
-        int maxItems = max(bins);                       // to find max height of histogram
 
-        // Printing stars
-        for (int rows = maxItems; rows > 0; rows--) {
-            for (int binNo = 0; binNo < 10; binNo++) {
-                if (bins[binNo] >= rows) {
-                    System.out.println("   *   ");      // 3 spaces padding
-                } else {
-                    System.out.println("       ");      // 7 spaces padding
-                }
+        // Find max height of histogram
+        for (int bin = 0; bin < 10; bin++) {
+            if (numInBin[bin] > max) {
+                max = numInBin[bin];
             }
-            System.out.println();                       // next row
+        }
+        // Print the stars
+        for (int row = max; row > 0; row--) {
+            for (int bin = 0; bin < 10; bin++) {
+                if (numInBin[bin] >= row)
+                    System.out.print("   *   ");
+                else
+                    System.out.print("       ");
+            }
+            System.out.println();
         }
 
-        // Printing labels
-        for (int binNo = 0; binNo < 10; binNo++) {
-            lowerBin = 10 * binNo;
-            upperBin = 10 * binNo + 9;
-            if (upperBin == 99) {
+        // Print the labels
+        for (int bin = 0; bin < 10; bin++) {
+            lowerBin = 10 * bin;
+            upperBin = 10 * bin + 9;
+            if (upperBin == 99)
                 upperBin = 100;
-            }
             System.out.printf("%3d-%-3d", lowerBin, upperBin);
         }
-    }
-
-    public static int max(int[] arr) {
-        int max = arr[0];
-        for (int idx = 0; idx < arr.length; idx++) {
-            if (arr[idx] > max) {
-                max = arr[idx];
-            }
-        }
-        return max;
     }
 }

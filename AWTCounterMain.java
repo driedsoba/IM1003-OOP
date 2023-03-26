b382027c6e5fee21eb6b3375e77587495fe75ce0
driedsoba <122796569+driedsoba@users.noreply.github.com>
2023-03-26 14:31:42 +0800

    Add files via upload

---
+++ AWTCounterMain.java
@@ -0,0 +1,9 @@
+public class AWTCounterMain {
+    public static void main(String[] args) {
+
+        // invoke constructor to setup GUI, with an instance
+        AWTCounter app = new AWTCounter();
+        // OR new AWTCounter(); for an anonymous instance
+
+    }
+}

445d34e17cba41a3be92932cc54591bd0c2b4b2f
driedsoba <122796569+driedsoba@users.noreply.github.com>
2023-04-30 19:50:20 +0800

    Add files via upload

---
+++ 2022/Sem2_2022_1b.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Sem2_2022_1b {
+
+    public static void main(String[] args) {
+        int size;
+        char label = 'A';
+
+        System.out.print("Enter the size: ");
+        Scanner scanner = new Scanner(System.in);
+        size = scanner.nextInt();
+
+        for (int rows = 1; rows <= size; rows++) {
+            System.out.println(label + ":");
+            for (int cols = 1; cols <= size; cols++) {
+                System.out.println( cols%2 == 0 ? "-" : "+");
+            }
+            System.out.println();
+            label++;
+        }
+    }
+}

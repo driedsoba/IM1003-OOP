404e0e9678818ade3057639083919c91fd1a3a91
driedsoba <122796569+driedsoba@users.noreply.github.com>
2023-04-30 20:39:55 +0800

    Add files via upload

---
+++ 2022/Sem2_2022_1c.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Sem2_2022_1c {
+
+    public static void main(String[] args) {
+
+        System.out.print("Enter a String: ");
+        Scanner scanner = new Scanner(System.in);
+        String inStr = scanner.nextLine();
+        System.out.println(dropVowels(inStr));
+    }
+
+    public static String dropVowels(String inStr) {
+        char[] vowels = {'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'};
+        char inChar;
+        boolean isVowel;
+        String outStr = "";
+
+        for (int i = 0; i < inStr.length(); i++) {
+            inChar = inStr.charAt(i);
+            isVowel = false;
+
+            for (int j = 0; j < vowels.length; j++) {
+                if (inChar == vowels[j]) {
+                    isVowel = true;
+                    break;
+                }
+            }
+            if (!isVowel) {
+                outStr += inChar;
+            }
+        }
+        return outStr;
+    }
+}

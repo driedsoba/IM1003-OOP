8dabee301d250be02404df7d1a7ef7c88704f6e7
driedsoba <122796569+driedsoba@users.noreply.github.com>
2023-04-30 23:15:36 +0800

    Add files via upload

---
+++ 2022/Point2D.java
@@ -0,0 +1,31 @@
+public class Point2D implements PointBase {
+
+    protected int x;
+    protected int y;
+
+    Point2D() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    Point2D(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String describe() {
+        return "(" + this.x + "," + this.y + ")";
+    }
+
+    @Override
+    public double distance() {  // distance from origin
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    }
+
+    public double distance(Point2D another) {   // distance from another point
+        int xDiff = this.x - another.x;
+        int yDiff = this.y - another.y;
+        return Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+    }
+}
---
+++ 2022/PointBase.java
@@ -0,0 +1,5 @@
+public interface PointBase {
+
+    public String describe();
+    public double distance();
+}
